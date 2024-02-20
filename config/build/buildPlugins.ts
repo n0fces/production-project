3a64ca8f03086ea196caee155a0f94f464b08ca6
n0fces <ilyaabzalov@gmail.com>
2024-02-20 21:01:26 +0300

    fix dev plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,8 +4,11 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
-export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
-	return [
+export function buildPlugins({
+	paths,
+	isDev,
+}: BuildOptions): webpack.WebpackPluginInstance[] {
+	const plugins = [
 		// чтобы использовать index.html из public в качестве шаблона при сборке, используем настройку template
 		new HtmlWebpackPlugin({
 			template: paths.html,
@@ -23,13 +26,21 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
 			// необязательно оформлять названия именно так, но это позволяет отделить глобальные переменные вебпака от других глобальных переменных
 			__IS_DEV__: JSON.stringify(isDev),
 		}),
-		// чтобы при изменению каких-нибудь стилей у нас происходили изменения, но страница не перезагружалась
-		// добавление плагина делает этот процесс лучше
-		new webpack.HotModuleReplacementPlugin(),
-		// чтобы смотреть размер пакетов общей сборки
-		new BundleAnalyzerPlugin({
-			// теперь бандл аналайзер не будет сразу открываться, а будет только ссылка на него, если мы хотим посмотреть этот отчет
-			openAnalyzer: false,
-		}),
 	];
+
+	// Данные плагины нам нужны только в режиме разработки
+	if (isDev) {
+		plugins.push(
+			// чтобы при изменению каких-нибудь стилей у нас происходили изменения, но страница не перезагружалась
+			// добавление плагина делает этот процесс лучше
+			new webpack.HotModuleReplacementPlugin(),
+			// чтобы смотреть размер пакетов общей сборки
+			new BundleAnalyzerPlugin({
+				// теперь бандл аналайзер не будет сразу открываться, а будет только ссылка на него, если мы хотим посмотреть этот отчет
+				openAnalyzer: false,
+			}),
+		);
+	}
+
+	return plugins;
 }
