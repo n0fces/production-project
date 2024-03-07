8abb60b4205c7b05e5934f8b8e8088c2327eed96
n0fces <ilyaabzalov@gmail.com>
2024-03-07 23:55:34 +0300

    fix gitignore, tests, screenshots

--- src/entities/Article/model/slice/articleDetailsSlice.test.ts
+++ src/entities/Article/model/slice/articleDetailsSlice.test.ts
@@ -1,43 +1,43 @@
-// import { fetchArticleById } from '../services/fetchArticleById/fetchArticleById';
-// import { ArticleDetailsScheme } from '../types/articleDetailsScheme';
-// import { articleDetailsReducer } from './articleDetailsSlice';
+import { fetchArticleById } from '../services/fetchArticleById/fetchArticleById';
+import { Article } from '../types/article';
+import { ArticleDetailsScheme } from '../types/articleDetailsScheme';
+import { articleDetailsReducer } from './articleDetailsSlice';
 
-// const data = { id: '1', title: 'subtitle' };
+const data = { id: '1', title: 'subtitle' };
 
-// describe('profileSlice.test', () => {
-// 	// Здесь мы тестируем асинхронные экшены. Все аналогично, только мы здесь явно указываем какое именно состояние тестируется у асинхронного экшена
-// 	test('test update profile service pending', () => {
-// 		const state: DeepPartial<ArticleDetailsScheme> = {
-// 			isLoading: false,
-// 			error: 'error',
-// 		};
+describe('profileSlice.test', () => {
+	test('test update profile service pending', () => {
+		const state: DeepPartial<ArticleDetailsScheme> = {
+			isLoading: false,
+			error: 'error',
+		};
 
-// 		expect(
-// 			articleDetailsReducer(
-// 				state as ArticleDetailsScheme,
-// 				fetchArticleById.pending
-// 			)
-// 		).toEqual({
-// 			isLoading: true,
-// 			error: undefined,
-// 		});
-// 	});
+		expect(
+			articleDetailsReducer(
+				state as ArticleDetailsScheme,
+				fetchArticleById.pending
+			)
+		).toEqual({
+			isLoading: true,
+			error: undefined,
+		});
+	});
 
-// 	test('test update profile service fullfiled', () => {
-// 		const state: DeepPartial<ArticleDetailsScheme> = {
-// 			isLoading: true,
-// 			error: 'error',
-// 		};
+	test('test update profile service fullfiled', () => {
+		const state: DeepPartial<ArticleDetailsScheme> = {
+			isLoading: true,
+			error: 'error',
+		};
 
-// 		expect(
-// 			articleDetailsReducer(
-// 				state as ArticleDetailsScheme,
-// 				fetchArticleById.fulfilled(data, '', '1')
-// 			)
-// 		).toEqual({
-// 			isLoading: false,
-// 			error: undefined,
-// 			data,
-// 		});
-// 	});
-// });
+		expect(
+			articleDetailsReducer(
+				state as ArticleDetailsScheme,
+				fetchArticleById.fulfilled(data as Article, '', '1')
+			)
+		).toEqual({
+			isLoading: false,
+			error: undefined,
+			data,
+		});
+	});
+});
