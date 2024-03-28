5a595cda42295b6153b319b9afbae3db6ab0073c
n0fces <ilyaabzalov@gmail.com>
2024-03-28 23:05:15 +0300

    fix

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 import { Story } from '@storybook/react';
 import { StateScheme, StoreProvider } from 'app/providers/StoreProvider';
 import { articleDetailsReducer } from 'entities/Article/model/slice/articleDetailsSlice';
@@ -18,12 +19,12 @@ const defaultAsyncReducers: ReducersList = {
 // не знаю, почему мы сделали так, что можем дополнительно пробрасывать еще асинхронных редьюсер, ведь сверху мы сделали объект, который по сути является костылем для всего этого. То есть мы передаем объект с асинхронными редьюсерами, чтобы у нас в сторибуках работало отображение нормально. Мы можем просто туда закидывать эти асинхроонные редьюсеры. Но нет, мы еще сделали возможность добавлять дополнительно асинхронный редьюсер. Короче странно решение для меня
 export const StoreDecorator =
 	(state: DeepPartial<StateScheme>, asyncReducers?: ReducersList) =>
-		(StoryComponent: Story) =>
-			(
-				<StoreProvider
-					initialState={state}
-					asyncReducers={{ ...defaultAsyncReducers, ...asyncReducers }}
-				>
-					<StoryComponent />
-				</StoreProvider>
-			);
+	(StoryComponent: Story) =>
+		(
+			<StoreProvider
+				initialState={state}
+				asyncReducers={{ ...defaultAsyncReducers, ...asyncReducers }}
+			>
+				<StoryComponent />
+			</StoreProvider>
+		);
