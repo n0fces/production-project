f7b19ca0b093ca6093ec291f5e3f2c46242432c1
n0fces <ilyaabzalov@gmail.com>
2024-08-10 22:05:35 +0300

    add generate slice script

---
+++ scripts/createSlice/index.js
@@ -0,0 +1,17 @@
+const createTemplate = require('./templates/createTemplate');
+
+const layer = process.argv[2];
+const sliceName = process.argv[3];
+
+const layers = ['features', 'entities', 'pages'];
+
+if (!layer || !layers.includes(layer)) {
+	throw new Error(`Укажите слой ${layers.join(' или ')}`);
+}
+
+if (!sliceName) {
+	throw new Error('Укажите название слайса');
+}
+
+// На самом деле я бы мог не реализовывать подобный скрипт, а сделать все через расширение folder templates. Там я могу задать сколь угодно сложную структуру/ы, а дальше выбирать, во-первых, какую структуру я хочу задать, а потом название, которое будет использоваться
+createTemplate(layer, sliceName);
