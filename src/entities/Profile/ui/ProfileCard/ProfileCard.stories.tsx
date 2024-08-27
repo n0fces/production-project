e4a6cc1daf8939ed74be6bb25b053b897b945e79
n0fces <ilyaabzalov@gmail.com>
2024-08-27 19:39:13 +0300

    fix storybook 2

--- src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
@@ -1,8 +1,8 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import avatar from '@/shared/assets/tests/storybook.jpg';
 import { Country } from '@/entities/Country';
 import { Currency } from '@/entities/Currency';
 import { ProfileCard } from './ProfileCard';
+import { Profile } from '../../model/types/profile';
 
 export default {
 	title: 'entities/ProfileCard',
@@ -12,22 +12,24 @@ export default {
 	},
 } as ComponentMeta<typeof ProfileCard>;
 
+const profile: Profile = {
+	first: 'Ilya',
+	lastname: 'Abzalov',
+	age: 21,
+	currency: Currency.RUB,
+	country: Country.Russia,
+	city: 'Saint-Petersburg',
+	username: 'admin',
+	avatar: 'https://image.spreadshirtmedia.net/image-server/v1/compositions/T6A1PA5835PT17X67Y83D175825012W17698H15087/views/1,width=550,height=550,appearanceId=1,backgroundColor=FFFFFF,noPt=true/wednesday-frog-mens-t-shirt.jpg',
+};
+
 const Template: ComponentStory<typeof ProfileCard> = (args) => (
 	<ProfileCard {...args} />
 );
 
 export const Primary = Template.bind({});
 Primary.args = {
-	data: {
-		first: 'Ilya',
-		lastname: 'Abzalov',
-		age: 21,
-		currency: Currency.RUB,
-		country: Country.Russia,
-		city: 'Saint-Petersburg',
-		username: 'admin',
-		avatar,
-	},
+	data: profile,
 };
 
 export const withError = Template.bind({});
