edca7055e4804faf175f669eebaf14b0b6ae1eef
n0fces <ilyaabzalov@gmail.com>
2024-09-02 22:40:49 +0300

    add bash script for deploy, getApiUrl function

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -4,7 +4,7 @@ import { Page } from '@/widgets/Page';
 const MainPage = () => {
 	const { t } = useTranslation();
 
-	return <Page data-testid="MainPage">{t('Главная страница')}</Page>;
+	return <Page data-testid="MainPage">1. {t('Главная страница')}</Page>;
 };
 
 export default MainPage;
